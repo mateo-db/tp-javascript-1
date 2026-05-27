258def1f1080a12f0426a7c6ac24918d78979167
Cabezas Mateo <cabezasluciano752@gmail.com>
2026-05-27 18:06:30 -0300

    feat: consigna 3 completada

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ carrera = 'Tecnicatura Superior en Desarrrollo de Software Multiplataforma'
 estoyCursando = 'si'
 
 
-console.log(`Soy ${nombre}, tengo ${edad} de edad. Estoy cursando la carreda de ${carrera}? La respuesta es ${estoyCursando}.`)
+console.log(`Soy ${nombre}, tengo ${edad} de edad. Estoy cursando la carrera de ${carrera}? La respuesta es ${estoyCursando}.`)
 
 const nota = 8;
 if (nota >= 6) {
     console.log("Aprobado")
 } else {
     console.log("Desaprobado")
+}
+
+edad = 15;
+if (edad < 13) {
+    console.log('Niño');
+} else if (edad >= 13 && edad <= 17) {
+    console.log('Adolescente');
+} else {
+    console.log('Adulto');
 }
