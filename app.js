a163c16ad26036b4741f9d74a45f78ef83f1a6f5
Cabezas Mateo <cabezasluciano752@gmail.com>
2026-05-27 17:01:13 -0300

    feat: consigna 2 completada

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('hola mundo')
+
+
+const nombre = 'Cabezas Mateo';
+let edad, carrera, estoyCursando;
+edad = 23;
+carrera = 'Tecnicatura Superior en Desarrrollo de Software Multiplataforma'
+estoyCursando = 'si'
+
+
+console.log(`Soy ${nombre}, tengo ${edad} de edad. Estoy cursando la carreda de ${carrera}? La respuesta es ${estoyCursando}.`)
+
+const nota = 8;
+if (nota >= 6) {
+    console.log("Aprobado")
+} else {
+    console.log("Desaprobado")
+}
