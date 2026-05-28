895a034f91335fbfbe90b402acf6932213405690
Cabezas Mateo <cabezasluciano752@gmail.com>
2026-05-27 23:04:44 -0300

    feat: consigna 9 completada

--- app.js
+++ app.js
@@ -78,3 +78,30 @@ console.log(`
     Promedio del alumno: ${promedioNotas}
     Estado: ${promedioNotas >= 6 ? 'Aprobó!' : 'No aprobó!'}
 `)
+
+//actividad 9
+const nombreAlumno = 'Filemón'
+const arrayNotas = [6, 9, 9, 8, 7]
+let promedioNotF
+let accNotF = 0
+let evaluacionPromedio
+
+for (let i = 0; i<arrayNotas.length; i++) {
+    accNotF = accNotF+arrayNotas[i]
+}
+
+promedioNotF = accNotF/arrayNotas.length
+
+if (promedioNotF>=7) {
+    evaluacionPromedio = 'Muy bueno!'
+} else if (promedioNotF>=6) {
+    evaluacionPromedio = 'Bueno'
+} else {
+    evaluacionPromedio = 'Desaprobado'
+}
+
+console.log(`
+    Nombre del alumno: ${nombreAlumno}
+    Promedio: ${promedioNotF}
+    Evaluación de Promedio: ${evaluacionPromedio}
+`)
