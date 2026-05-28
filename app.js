e0f2e860a8bf3dcbf888998cb1d3b9c7448732bd
Cabezas Mateo <cabezasluciano752@gmail.com>
2026-05-27 22:45:38 -0300

    feat: consigna 8 completada

--- app.js
+++ app.js
@@ -61,3 +61,20 @@ console.log(`
     Nota: ${alumno.nota}
     Estado: ${nota >= 6 ? 'Está aprobado!' : 'Está desaprobado'}
 `)
+
+//actividad 8
+const notas = [6, 4, 5, 8, 7];
+let accNotas = 0
+let promedioNotas
+
+for (let i = 0; i<notas.length; i++) {
+    console.log(`${accNotas = accNotas+notas[i]}`)
+
+}
+
+promedioNotas = accNotas/notas.length
+
+console.log(`
+    Promedio del alumno: ${promedioNotas}
+    Estado: ${promedioNotas >= 6 ? 'Aprobó!' : 'No aprobó!'}
+`)
